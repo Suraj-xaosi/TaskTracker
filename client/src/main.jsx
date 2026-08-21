a39b01040e13a0eab46082356df6d9dab9a41fc3
Suraj-xaosi <suraj001001001@gmail.com>
2026-08-21 19:36:55 +0530

    added script

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -33,7 +33,14 @@ const router = createBrowserRouter([
     ),
   },
 ])
-
+//----------------------------------------------
+const script = document.createElement('script')
+script.src = "https://webanly-dashboard.vercel.app/script.js"
+script.setAttribute('data-domain-name', 'funworkcycle-dashboard.render.com738')
+script.setAttribute('data-api-key', '3a342f65-a05c-450e-95ca-ddb3bc5c3897')
+script.async = true
+document.head.appendChild(script)
+// ----------------------------------------------
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppContextProvider>
